e910806006fda9c7fcf4df4de07ec10a0b9e1c75
Lucifer <814255527@qq.com>
2021-03-12 09:42:47 +0800

    管理员登录

---
+++ CavalryJurisprudence/WebUI/UI/01AdminLogin.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="01AdminLogin.aspx.cs" Inherits="AdminLogin_AdminLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <link rel="stylesheet" href="01AdminBlock/AdminLoginCSS/style.css" />
+
+    <div id="logo" class="auto-style3">
+        <h1><i>CAVALRY JURISPRUDENCE</i></h1>
+    </div>
+
+    <section class="Cavalry-login">
+
+        <form id="form1" runat="server">
+            <div id="fade-box">
+                <asp:TextBox ID="TBAdministratorAccount" runat="server" placeholder="请输入管理人员账号" required="输入账户"></asp:TextBox>
+                <asp:TextBox ID="TBAdministratorPassword" runat="server" placeholder="请输入管理人员密码" required="输入密码" TextMode="Password"></asp:TextBox>
+                <asp:Button ID="BTNLogin" runat="server" Text="登录" OnClick="BTNLogin_Click" />
+            </div>
+        </form>
+
+        <div class="hexagons">
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+        </div>
+    </section>
+
+    <div id="circle1">
+        <!--光环-->
+        <div id="inner-cirlce1">
+            <h2></h2>
+        </div>
+    </div>
+
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</body>
+</html>
---
+++ CavalryJurisprudence/WebUI/UI/01AdminLogin.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using Entity;
+
+public partial class AdminLogin_AdminLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTNLogin_Click(object sender, EventArgs e)
+    {
+        /**
+         *本界面实现的功能：
+         *1.实现了对于管理人员用户类型的判断，从而实现了单登录页面，多用户登录
+         *2.实现了基本的登录功能，比如账户、密码判断
+         */
+        AdministratorAccountClassificationJudgement();
+    }
+
+    private void AdministratorAccountClassificationJudgement()
+    {
+        /**
+         * 用户类别判断算法
+         * 
+         * 以名称后面的【A】或【66】来区分是 【管理员】或【董事会】
+         */
+        int iInputStringJudgement = 0;
+        string sAdministratorAccount = TBAdministratorAccount.Text.ToString();//获取前台传来的【账户】信息
+        for (int iCounter = 0; iCounter < sAdministratorAccount.Length; iCounter++)
+        {
+            if (sAdministratorAccount[iCounter] <= 0 || sAdministratorAccount[iCounter] > 9)
+            {
+                iInputStringJudgement++;
+            }
+            else
+            {
+                iInputStringJudgement = 0;
+            }
+        }
+        string sClassificationJudgementJudgeString = sAdministratorAccount.Substring(sAdministratorAccount.Length - 1, 1);//取最后一个字符以验证
+
+        if (sClassificationJudgementJudgeString == 'A'.ToString() || sClassificationJudgementJudgeString == 'a'.ToString())
+        {
+            AdminLogin();//转到管理员登录函数
+        }
+        else if (sClassificationJudgementJudgeString == "6" && iInputStringJudgement > 0)
+        {
+            BoardLogin();//转到董事会成员登陆函数
+        }
+        else
+        {
+            Response.Write("<script>alert('信息不存在！！')</script>");
+        }
+    }
+    private void AdminLogin()
+    {
+        /**
+         * 查询是否存在 管理员
+         */
+        string sPasswordReceive = TBAdministratorPassword.Text;
+
+        AdminInfoBusiness GetAdminInfo = new AdminInfoBusiness();
+        AdminInfoEntity AdminInfo = new AdminInfoEntity();
+        AdminInfo = GetAdminInfo.GetAdminInfoByAdminAccount();
+
+        int iReturnValue = (int)GetAdminInfo.AdminExistJudgementByAdminAccount(TBAdministratorAccount.Text);
+        if (iReturnValue > 0)
+        {
+            MultipurposeMultiplexingClass DataEncrypt = new MultipurposeMultiplexingClass();
+            string sEncryptPassword = DataEncrypt.DataEncryptMethod(TBAdministratorPassword.Text);
+
+            string sAdminPassword = AdminInfo.saadminPasswords[0];
+            MultipurposeMultiplexingClass AttractedWarningLevel = new MultipurposeMultiplexingClass();
+            int iReturnLevel = AttractedWarningLevel.SimilarityDetect(sEncryptPassword, sAdminPassword);//相似度检测算法，用以防止爆破攻击
+
+            if (iReturnLevel == 100)
+            {
+                Response.Redirect("~/01AdminCentre.aspx");
+            }
+            else
+            {
+                Response.Redirect("~/0000.aspx");
+            }
+        }
+        else
+        {
+            Response.Redirect("~/0000.aspx");
+        }
+    }
+    private void BoardLogin()
+    {
+        /**
+         * 查询是否存在 董事会成员
+         * 
+         */
+        BoardInfoBusiness BoardLogin = new BoardInfoBusiness();
+        int iJudgementValue = (int)BoardLogin.BoardExistJudgement(long.Parse(TBAdministratorAccount.Text), TBAdministratorPassword.Text);
+        if (iJudgementValue > 0)
+        {
+            Session["BoardMemberID"] = TBAdministratorAccount.Text;
+            Response.Redirect("~/02BoardCentre.aspx");
+        }
+        else
+        {
+            Response.Redirect("~/0000.aspx");
+        }
+    }
+}
