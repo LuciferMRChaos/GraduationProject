93cc4490a49909c40dc603524b1b47c42f9411c0
Lucifer <814255527@qq.com>
2021-03-26 13:27:20 +0800

    悬赏细节

---
+++ CavalryJurisprudence/WebUI/UI/09WantedDetail.aspx
@@ -0,0 +1,133 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="09WantedDetail.aspx.cs" Inherits="_09WantedDetail" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <style type="text/css">
+        .auto-style4 {
+            height: 31px;
+            text-align: left;
+        }
+        .auto-style5 {
+            height: 162px;
+            text-align: left;
+        }
+        .auto-style6 {
+            text-align: center;
+            width: 396px;
+        }
+        .auto-style7 {
+            height: 84px;
+            text-align: center;
+        }
+        .auto-style8 {
+            width: 100%;
+            height: 112px;
+        }
+        .auto-style9 {
+            margin-left: 0px;
+        }
+        .auto-style10 {
+            width: 100%;
+            height: 261px;
+        }
+        .auto-style11 {
+            width: 151px;
+        }
+        .auto-style12 {
+            width: 181px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <table class="auto-style1">
+        <tr>
+            <td class="auto-style6" rowspan="2">
+                悬赏人姓名：<asp:Label ID="LBLClientName" runat="server" Text="悬赏人姓名"></asp:Label><br />
+                <br />
+                <asp:Button ID="BTNWantedInfoUpdate" runat="server" Text="修改悬赏信息" OnClick="BTNWantedInfoUpdate_Click" />
+            </td>
+            <td class="auto-style4">
+                悬赏题目：<asp:TextBox ID="TBWantedTitle" runat="server"></asp:TextBox>
+                &nbsp;
+                悬赏金额：<asp:DropDownList runat="server" ID="DDLWantedBounty"></asp:DropDownList>万元&nbsp;
+                悬赏领域：<asp:Label ID="LBLWantedField" runat="server" Text="领域"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style5">
+                <asp:TextBox ID="TBWantedContent" runat="server" Height="147px" Width="961px"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+    <link rel="stylesheet" href="09WantedBlock/WantedDetail/WantedDetailCSS/style.css">
+
+
+
+    <div id="overlay">
+        <!--在这里可以放单独的东西，上下滑动而其它地方不受影响-->
+
+        <asp:DataList ID="DLWantedAnswerInfo" runat="server" DataSourceID="SqlDataSource1" DataKeyField="AnswerID" OnItemCommand="DLWantedAnswerInfo_ItemCommand" OnItemCreated="DLWantedAnswerInfo_ItemCreated" >
+            <ItemTemplate>
+                <table class="auto-style10">
+                    <tr>
+                        <td class="auto-style11">
+                            <asp:Image ID="IMGResponderImage" runat="server" Height="213px" Width="153px" ImageUrl='<%# Eval("CounsellorImage") %>' />
+                        </td>
+                        <td colspan="4">
+                            <asp:TextBox ID="TBRespondContent" runat="server" Height="212px" Width="676px" ReadOnly="True" Text='<%# Eval("RespondContent") %>' TextMode="MultiLine"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style11">
+                            <asp:Label ID="LBLRespondName" runat="server" Text='<%# Eval("CounsellorName") %>'></asp:Label>
+                        </td>
+                        <td class="auto-style12">
+                            <asp:Label ID="LBLAnswer" runat="server" Text="最佳答案"  Visible='<%# Convert .ToInt32 (Eval("SelectedAsAnswer"))>0?true:false %>'></asp:Label>
+                        </td>
+                        <td>
+                            点赞数：<br /> <asp:Button ID="BTNRespondLikedCount" runat="server" Text='<%# Eval("RespondLikedCount") %>' CommandName="BTNRespondLikedCount"/>
+                        </td>
+                        <td>
+                            点踩数：<br /> 
+                            <asp:Button ID="BTNRespondDislikedCount" runat="server" Text='<%# Eval("RespondDislikedCount") %>' CommandName="BTNRespondDislikedCount"/>
+                        </td>
+                        <td>
+                            <asp:Button ID="BTNSelectAsAnswer" runat="server" CommandArgument='<%# Eval("AnswerID") %>' Text="选为答案" CommandName="BTNSelectAsAnswer" />
+                            <br />
+                            <asp:Label ID="LBLAnswerID" runat="server" Text='<%# Eval("AnswerID") %>' Visible="false" CommandName="LBLAnswerID" ></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+                <br />
+            </ItemTemplate>
+        </asp:DataList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:CavalryJurisprudenceConnectionString %>" SelectCommand="SELECT [CounsellorName],[AnswerID],[SelectedAsAnswer], [CounsellorImage], [RespondContent], [RespondLikedCount], [RespondDislikedCount] FROM [WantedAnswerInfo],[CounsellorInfo] WHERE [CounsellorID]=[ResponderIDFromCounsellorInfo] and ([WantedIDFromWantedQuestionInfo] = @WantedIDFromWantedQuestionInfo)">
+            <SelectParameters>
+                <asp:QueryStringParameter Name="WantedIDFromWantedQuestionInfo" QueryStringField="WantedID" Type="Int64" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+
+    </div>
+
+    <div class="bgContain">
+        <!--背景图片-->
+        <img src="WebImages/09WantedDetailBackgroundImage.jpg" alt="背景图片" />
+    </div>
+
+    <div>
+        
+        <table class="auto-style8">
+            <tr>
+                <td class="auto-style7">
+                    <asp:TextBox ID="TBReply" runat="server" CssClass="auto-style9" Height="75px" Width="961px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">
+                    <asp:Button ID="BTNReply" runat="server" Text="我来回答" />
+                </td>
+            </tr>
+        </table>
+        
+    </div>
+</asp:Content>
+
---
+++ CavalryJurisprudence/WebUI/UI/09WantedDetail.aspx.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using Entity;
+
+public partial class _09WantedDetail : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        WantedPromoterDefaultInfoDisplay();
+    }
+
+    private void WantedPromoterDefaultInfoDisplay()
+    {
+        long lWantedID = long.Parse(Request.QueryString["WantedID"].ToString());//接收Wantedlist传来的ID
+        WantedInfoBusiness WantedQuestionDetailsInfoSearch = new WantedInfoBusiness();
+        WantedQuestionDetailsInfoEntity WantedQuestionInfo = new WantedQuestionDetailsInfoEntity();
+
+        long UsersID = long.Parse(Session["UsersID"].ToString());
+        TBWantedTitle.ReadOnly = true;
+        TBWantedContent.ReadOnly = true;
+        DDLWantedBounty.Enabled = false;
+        TBReply.Visible = false;
+        BTNReply.Visible = false;
+        BTNWantedInfoUpdate.Visible = false;
+        if (UsersID % 100 == 67)
+        {//如果是律师登录
+            TBReply.Visible = true;
+            BTNReply.Visible = true;
+        }
+        else if(UsersID % 100 == 99)
+        {
+            //附加：如果是当前问题的提问人,则可以修改悬赏的信息
+            long lPromoterID = WantedQuestionDetailsInfoSearch.GetWantedPromoterIDByWantedID(lWantedID);
+            if (UsersID == lPromoterID)
+            {
+                TBWantedTitle.ReadOnly = false;
+                TBWantedContent.ReadOnly = false;
+                DDLWantedBounty.Enabled = true;
+                BTNWantedInfoUpdate.Visible = true;
+            }
+            TBReply.Visible = false;
+            BTNReply.Visible = false;
+        }
+        else
+        {
+            //游客登录
+        }
+
+        if (!IsPostBack)
+        {
+            WantedQuestionInfo = WantedQuestionDetailsInfoSearch.WantedQuestionDetailsInfoByID(lWantedID);
+            LBLClientName.Text = WantedQuestionInfo.sclientName;
+            TBWantedContent.Text = WantedQuestionInfo.swantedContent;
+            DDLWantedBounty.SelectedValue = (WantedQuestionInfo.lwantedBounty / 10000).ToString();
+
+            TBWantedTitle.Text = WantedQuestionInfo.swantedTitle;
+            LBLWantedField.Text = WantedQuestionInfo.swantedField;
+
+            for (long iWantedBounty = WantedQuestionInfo.lwantedBounty / 10000; iWantedBounty < 101; iWantedBounty += 5)//绑定1~x万元
+            {
+                DDLWantedBounty.Items.Add(iWantedBounty.ToString());
+            }
+        }
+        
+        
+    }
+    
+    protected void DLWantedAnswerInfo_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        Button BTNSelectAsAnswer = (Button)e.Item.FindControl("BTNSelectAsAnswer");
+        Label LBLAnswerID = (Label)e.Item.FindControl("LBLAnswerID");
+        WantedInfoBusiness SetTargetAsAnswer = new WantedInfoBusiness();
+        int iReturnedValue = 0;
+        if (e.CommandName== "BTNSelectAsAnswer")
+        {
+            //当客户登录时，若没有选定的答案，则点击后可设置最佳答案
+            iReturnedValue = SetTargetAsAnswer.SetAsAnswer(long.Parse(LBLAnswerID.Text));
+            if (iReturnedValue > 0)
+            {
+                long lWantedID = long.Parse(Request.QueryString["WantedID"].ToString());
+                CounsellorInfoBusiness CounsellorInfo = new CounsellorInfoBusiness();
+                ClientInfoBusiness ClientInfo = new ClientInfoBusiness();
+
+                long lCounsellorID = CounsellorInfo.GetCounsellorIDByAnswerID(long.Parse(LBLAnswerID.Text));
+                long lClientID = ClientInfo.GetClientIDByWantedID(lWantedID);
+
+                WantedInfoBusiness WantedQuestionDetailsInfoSearch = new WantedInfoBusiness();
+                WantedQuestionDetailsInfoEntity WantedQuestionInfo = new WantedQuestionDetailsInfoEntity();
+                WantedQuestionInfo = WantedQuestionDetailsInfoSearch.WantedQuestionDetailsInfoByID(lWantedID);
+                long lBountyMoney= WantedQuestionInfo.lwantedBounty;
+                LBLWantedField.Text = lBountyMoney.ToString();
+                ClientInfo.ClientWantedBountyMoney(lBountyMoney, lClientID);
+                CounsellorInfo.CounsellorWalletMoneyUpdate(lCounsellorID, lBountyMoney);
+
+                Response.Write("<script>alert('成功设置为答案！')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('设置为答案失败！')</script>");
+            }
+        }
+        /**
+         * 任何身份都可以点赞，包括游客
+         */
+        else if(e.CommandName== "BTNRespondLikedCount")
+        {
+            //点了点赞后，让点赞数+1
+            iReturnedValue = SetTargetAsAnswer.RespondLiked(long.Parse(LBLAnswerID.Text));
+            if (iReturnedValue > 0)
+            {
+                Response.Write("<script>alert('您赞了此答案！')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('点赞失败！！')</script>");
+            }
+        }
+        else if(e.CommandName== "BTNRespondDislikedCount")
+        { //点了点踩后，让点踩数+1
+            iReturnedValue = SetTargetAsAnswer.RespondDisliked(long.Parse(LBLAnswerID.Text));
+            if (iReturnedValue > 0)
+            {
+                Response.Write("<script>alert('您踩了此答案！')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('点踩失败！')</script>");
+            }
+        }
+    }
+
+    protected void DLWantedAnswerInfo_ItemCreated(object sender, DataListItemEventArgs e)
+    {
+        long lWantedID = long.Parse(Request.QueryString["WantedID"].ToString());
+        Button BTNSelectAsAnswer = (Button)e.Item.FindControl("BTNSelectAsAnswer");
+
+        WantedInfoBusiness AnswerAmount = new WantedInfoBusiness();
+        int iResultJudgement = int.Parse(AnswerAmount.AnswerState(lWantedID).ToString());
+
+        long UsersID = long.Parse(Session["UsersID"].ToString());
+        if (iResultJudgement > 0||UsersID % 100 != 99)
+        {
+            BTNSelectAsAnswer.Visible = false;
+        }
+    }
+
+    protected void BTNWantedInfoUpdate_Click(object sender, EventArgs e)
+    {
+        long lWantedID = long.Parse(Request.QueryString["WantedID"].ToString());
+
+        WantedInfoBusiness WantedInfoUpdate = new WantedInfoBusiness();
+
+        string sWantedTitle = TBWantedTitle.Text;
+        string sWantedCountent = TBWantedContent.Text;
+        long lWantedBounty = long.Parse(DDLWantedBounty.SelectedValue)*10000;
+
+
+        int iResultJudgement = WantedInfoUpdate.WantedInfoUpdate(sWantedTitle, sWantedCountent, lWantedBounty,lWantedID);
+
+        if (iResultJudgement > 0)
+        {
+            Response.Write("<script>alert('您已成功修改问题！')</script>");
+        }
+    }
+}
