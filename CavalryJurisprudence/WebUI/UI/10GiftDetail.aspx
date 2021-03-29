1a7437ed6e1498d5839b6dbb09f45f79550ca6e2
Lucifer <814255527@qq.com>
2021-03-29 23:16:07 +0800

    礼品详情

---
+++ CavalryJurisprudence/WebUI/UI/10GiftDetail.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="10GiftDetail.aspx.cs" Inherits="_10GiftDetail" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <link rel="stylesheet" href="10GiftBlock/GiftDetail/GiftDetailCSS/style.css">
+    <div class="wrapper">
+        <div class="product-img">
+            <asp:Image ID="IMGGiftImage" runat="server" Height="520" Width="360" ImageUrl="~/测试.jpg" />
+        </div>
+        <div class="product-info">
+            <div class="product-text">
+                <h1>
+                    <asp:Label ID="LBLGiftName" runat="server" Text="礼品名"></asp:Label>
+                </h1>
+                <h2>
+                    <asp:Label ID="LBLGiftTips" runat="server" Text="礼品简介" Font-Size="XX-Small"></asp:Label>
+                </h2>
+
+                <asp:Label ID="LBLGiftInfo" runat="server" Text="礼品介绍，可自动换行测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试" Font-Size="Small"></asp:Label>
+            </div>
+            <div class="product-price-btn">
+                剩余<asp:Label ID="LBLGiftAmount" runat="server" Text="x"></asp:Label>件
+          
+          <br />
+                仅需<asp:Label ID="LBLGiftPrice" runat="server" Text="价格" Font-Size="X-Small"></asp:Label>积分
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="BTNGiftPurchase" runat="server" Text="立即抢购" OnClick="BTNGiftPurchase_Click" />
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ CavalryJurisprudence/WebUI/UI/10GiftDetail.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using Entity;
+
+public partial class _10GiftDetail : System.Web.UI.Page
+{
+    long lGiftID;
+    int iGiftPrice;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GiftDetailDisplay();
+    }
+    private void GiftDetailDisplay()
+    {
+        lGiftID = long.Parse(Request.QueryString["GiftID"].ToString());
+        GiftInfoBusiness GiftDetails = new GiftInfoBusiness();
+        GiftInfoEntity GiftDetailsInfo = new GiftInfoEntity();
+        if (!IsPostBack)
+        {
+            GiftDetailsInfo = GiftDetails.GetGiftInfoByID(lGiftID);
+            iGiftPrice = GiftDetailsInfo.igiftPrice;
+            LBLGiftName.Text = GiftDetailsInfo.sgiftName;
+            LBLGiftTips.Text = GiftDetailsInfo.sgiftTips;
+            LBLGiftInfo.Text = GiftDetailsInfo.sgiftInfo;
+            LBLGiftAmount.Text = GiftDetailsInfo.igiftAmount.ToString();
+            LBLGiftPrice.Text = iGiftPrice.ToString();
+            IMGGiftImage.ImageUrl = GiftDetailsInfo.sgiftImage;
+        }
+    }
+
+    protected void BTNGiftPurchase_Click(object sender, EventArgs e)
+    {
+        UsersLoginDetect();
+    }
+    private void UsersLoginDetect()
+    {
+        long lUserID = long.Parse(Session["UsersID"].ToString());
+        if (lUserID == 0)
+        {
+            Response.Write("<script>alert('请先登录!')</script>");
+        }
+        else if(lUserID % 100 == 67)
+        {
+            Response.Write("<script>alert('本站律师不得兑换奖品!')</script>");
+        }
+        else if(lUserID % 100 == 99)
+        {
+            long lGiftID = long.Parse(Request.QueryString["GiftID"].ToString());
+            GiftInfoBusiness GiftDetails = new GiftInfoBusiness();
+            GiftInfoEntity GiftDetailsInfo = new GiftInfoEntity();
+            GiftDetailsInfo = GiftDetails.GetGiftInfoByID(lGiftID);
+
+            ClientGiftPurchase(GiftDetailsInfo.igiftPrice);
+        }
+    }
+    private void ClientGiftPurchase(int iGiftPrice)
+    {
+        ClientInfoBusiness ClientRemainPoints = new ClientInfoBusiness();
+        ClientInfoEntity ClientInfo = new ClientInfoEntity();
+        ClientInfo = ClientRemainPoints.GetClientInfoByID(long.Parse(Session["UsersID"].ToString()));
+
+        if ((long)iGiftPrice - (long)ClientInfo.lclientPoints<0)
+        {
+            GiftTradeBusiness GiftTrade = new GiftTradeBusiness();
+            GiftTrade.ClientGiftPurchase(long.Parse(Request.QueryString["GiftID"].ToString()), long.Parse(Session["UsersID"].ToString()));
+            int iReturnValue=ClientRemainPoints.ClientGiftPurchaseByPoints(iGiftPrice, long.Parse(Session["UsersID"].ToString()));
+            if (iReturnValue > 0)
+            {
+                Response.Write("<script>alert('感谢您的兑换！')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('程序出错，兑换失败！')</script>");
+            }
+        }
+        else
+        {
+            Response.Write("<script>alert('您的剩余积分不足!')</script>");
+        }
+    }
+}
